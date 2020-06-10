9b3d1f1b330f24bbe0b66aa3ee09415844e18abd
adamus <adomas.lingevicius@gmail.com>
2020-06-10 16:16:18 +0300

    added argparse

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+import argparse
+
 import speech_recognition as sr
 from pynput.keyboard import Key, Controller
 
@@ -24,55 +26,63 @@ def vUp():
 def vDown():
     keypress(Key.down)
     
-
-def mainfunctions(source):
-    audio = r.listen(source, phrase_time_limit=5) 
-    try:
-        user = r.recognize_google(audio)
-
-        print(user)
-
-        if 'play' in user:
-            play()
-            # r.adjust_for_ambient_noise(source)
-        elif 'stop' in user:
-            play()
-            # r.adjust_for_ambient_noise(source)
-        elif 'full screen' in user:
-            fullscreen()
-        elif 'volume up' in user:
-            vUp()
-        elif 'volume down' in user:
-            vDown()
-        elif 'forward' in user:
-            times = user.split(' ')[2]
+def run(args):
+    r = sr.Recognizer()
+    r.energy_threshold = args.threshold
+    times = args.skip
+    with sr.Microphone() as source:
+        while 1:
+            audio = r.listen(source, phrase_time_limit=5) 
             try:
-                times = int(times)
-            except ValueError:
-                times = 1
-            for _ in range(int(times)):
-                forward()
-            play()
-        elif 'backwards' in user:
-            times = user.split(' ')[2]
-            try:
-                times = int(times)
-            except ValueError:
-                times = 1
-            for _ in range(int(times)):
-                backward()
-            play()
+                user = r.recognize_google(audio)
 
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-        
+                print(user)
+
+                if 'play' in user:
+                    play()
+                elif 'stop' in user:
+                    play()
+                elif 'full screen' in user:
+                    fullscreen()
+                elif 'volume up' in user:
+                    vUp()
+                elif 'volume down' in user:
+                    vDown()
+                elif 'forward' in user:
+                    for _ in range(int(times)):
+                        forward()
+                    play()
+                elif 'backward' in user:
+                    for _ in range(int(times)):
+                        backward()
+                    play()
+
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Send key inputs using voice commands.')
+    parser.add_argument(
+        '-thresh', 
+        help='Set microphone sensitivity at which it will react to sound. Higher number - lower sensitivity. Default - 4000.', 
+        dest='threshold', 
+        type=int, 
+        default=4000, 
+        required=False)
+    parser.add_argument(
+        '-skip', 
+        help='Set the amount of skips (forward and backward) to do after you say "forward" or "backward". Default - 4', 
+        dest='skip', 
+        type=int, 
+        default=4, 
+        required=False)
+    parser.set_defaults(func=run)
+    args = parser.parse_args()
+    args.func(args)
+    
 
 if __name__ == '__main__':
-    r = sr.Recognizer()
-    r.energy_threshold = 8500
-    with sr.Microphone() as source:
-        # audio = r.listen_in_background(source,)
-        while 1:
-            mainfunctions(source)
+    main()
